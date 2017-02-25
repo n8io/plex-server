fa60dc66d0fefdc333a1bd89cb3e8dcd650e47f1
Nate Clark <n8@n8io.com>
2017-02-25 15:13:18 -0500

    Add some smart config checks in bashrc script

--- setup/00.bashrc.sh
+++ setup/00.bashrc.sh
@@ -4,14 +4,28 @@ set -e
 BASHRC="/root/.bashrc"
 CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+PREV_REMOTE_DIR="${ENC_DIR_REMOTE:-/encrypted}"
+PREV_LOCAL_DIR="${ENC_DIR_LOCAL:-/mnt/x}"
+PREV_ENCFS_CREDS="${ENCFS_CREDS:-/.encfs}"
+PREV_ENCFS6_CONFIG="${ENCFS6_CONFIG:-/encfs.xml}"
+
 prompt_for_creds() {
-  echo -n "  Enter your Amazon Cloud Drive encrypted directory [/encrypted]: "
+  echo -n "  Enter your Amazon Cloud Drive encrypted directory [${PREV_REMOTE_DIR}]: "
   read ENC_DIR_REMOTE
-  echo -n "  Enter the root path where you want everything to be mounted [/mnt/x]: "
+  echo -n "  Enter the absolute path to where you want everything to be mounted [${PREV_LOCAL_DIR}]: "
   read ENC_DIR_LOCAL
+  echo -n "  Enter the absolute path to your ENCFS encryption password file [${PREV_ENCFS_CREDS}]: "
+  read ENCFS_CREDS
+  echo -n "  Enter the absolute path to your ENCFS encryption config file [${PREV_ENCFS6_CONFIG}]: "
+  read ENCFS6_CONFIG
 
   wipe_old # remove previous settings
-  write_bashrc "${ENC_DIR_REMOTE:-/encrypted}" "${ENC_DIR_LOCAL:-/mnt/x}" # write new values
+  write_bashrc \ # write new values
+    "${ENC_DIR_REMOTE:-${PREV_REMOTE_DIR}}" \
+    "${ENC_DIR_LOCAL:-${PREV_LOCAL_DIR}}" \
+    "${ENCFS_CREDS:-${PREV_ENCFS_CREDS}}" \
+    "${ENCFS6_CONFIG:-${PREV_ENCFS6_CONFIG}}" \
+    ;
 
   return 0
 }
@@ -31,11 +45,11 @@ cat <<EOT >> "$BASHRC"
 #plex-settings-start
 
 export \\
-  ACD_MOUNT_DIR="${2}" \\
+  ACD_MOUNT_DIR="${2:-/encrypted}" \\
   EDITOR="nano" \\
-  ENC_DIR_REMOTE="${1}" \\
-  ENCFS_CREDS="/.encfs" \\
-  ENCFS6_CONFIG="/encfs.xml" \\
+  ENC_DIR_REMOTE="${1:-remote}" \\
+  ENCFS_CREDS="${3:-/.encfs}" \\
+  ENCFS6_CONFIG="${4:-/encfs.xml}" \\
   ENCFS_BIN="/usr/bin/encfs" \\
   ENCFS_CTL_BIN="/usr/bin/encfsctl" \\
   PLEX_CODE_DIR="$CWD" \\
