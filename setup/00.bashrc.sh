77cfed37647bce07da2b7975f45baa64f5e1f588
Nate Clark <n8@n8io.com>
2017-02-25 15:20:04 -0500

    Fix bugs

--- setup/00.bashrc.sh
+++ setup/00.bashrc.sh
@@ -5,26 +5,26 @@ BASHRC="/root/.bashrc"
 CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 PREV_REMOTE_DIR="${ENC_DIR_REMOTE:-/encrypted}"
-PREV_LOCAL_DIR="${ENC_DIR_LOCAL:-/mnt/x}"
+PREV_MOUNT_DIR="${MOUNT_DIR:-/mnt/x}"
 PREV_ENCFS_CREDS="${ENCFS_CREDS:-/.encfs}"
 PREV_ENCFS6_CONFIG="${ENCFS6_CONFIG:-/encfs.xml}"
 
 prompt_for_creds() {
   echo -n "  Enter your Amazon Cloud Drive encrypted directory [${PREV_REMOTE_DIR}]: "
   read ENC_DIR_REMOTE
-  echo -n "  Enter the absolute path to where you want everything to be mounted [${PREV_LOCAL_DIR}]: "
-  read ENC_DIR_LOCAL
-  echo -n "  Enter the absolute path to your ENCFS encryption password file [${PREV_ENCFS_CREDS}]: "
+  echo -n "  Enter the path to where you want everything to be mounted [${PREV_MOUNT_DIR}]: "
+  read MOUNT_DIR
+  echo -n "  Enter the path to your ENCFS encryption password file [${PREV_ENCFS_CREDS}]: "
   read ENCFS_CREDS
-  echo -n "  Enter the absolute path to your ENCFS encryption config file [${PREV_ENCFS6_CONFIG}]: "
+  echo -n "  Enter the path to your ENCFS encryption config file [${PREV_ENCFS6_CONFIG}]: "
   read ENCFS6_CONFIG
 
   wipe_old # remove previous settings
   write_bashrc \ # write new values
     "${ENC_DIR_REMOTE:-${PREV_REMOTE_DIR}}" \
-    "${ENC_DIR_LOCAL:-${PREV_LOCAL_DIR}}" \
     "${ENCFS_CREDS:-${PREV_ENCFS_CREDS}}" \
     "${ENCFS6_CONFIG:-${PREV_ENCFS6_CONFIG}}" \
+    "${MOUNT_DIR:-${PREV_MOUNT_DIR}}" \
     ;
 
   return 0
@@ -45,23 +45,23 @@ cat <<EOT >> "$BASHRC"
 #plex-settings-start
 
 export \\
-  ACD_MOUNT_DIR="${2:-/encrypted}" \\
   EDITOR="nano" \\
-  ENC_DIR_REMOTE="${1:-remote}" \\
-  ENCFS_CREDS="${3:-/.encfs}" \\
-  ENCFS6_CONFIG="${4:-/encfs.xml}" \\
+  ENC_DIR_REMOTE="${1:-/encrypted}" \\
+  ENCFS_CREDS="${2:-/.encfs}" \\
+  ENCFS6_CONFIG="${3:-/encfs.xml}" \\
   ENCFS_BIN="/usr/bin/encfs" \\
   ENCFS_CTL_BIN="/usr/bin/encfsctl" \\
+  MOUNT_DIR="${4:-/mnt/x}" \\
   PLEX_CODE_DIR="$CWD" \\
   RCLONE_BIN="/usr/sbin/rclone" \\
   ;
 
 export \\
-  ENC_DIR_LOCAL="\${ACD_MOUNT_DIR}/encrypted" \\
-  DEC_DIR_LOCAL="\${ACD_MOUNT_DIR}/decrypted" \\
+  ENC_DIR_LOCAL="\${MOUNT_DIR}/encrypted" \\
+  DEC_DIR_LOCAL="\${MOUNT_DIR}/decrypted" \\
   ;
 
-. "${CWD}/../scripts/helper-functions.sh"
+source "${CWD}/../scripts/helper-functions.sh"
 
 cd "${CWD}/.."
 
