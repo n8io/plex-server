d3dad4d69eb74bcb79086fe25a9dadf9c3f31f88
Nate Clark <n8@n8io.com>
2017-02-26 15:29:49 -0500

    Updates

--- scripts/mount-check.sh
+++ scripts/mount-check.sh
@@ -12,14 +12,14 @@ COUNT=$("$FIND_BIN" "${DEC_DIR_LOCAL}" -maxdepth 1 -type d | wc -l)
 if [ "$COUNT" -eq "1" ]; then
   log_error "Drives are not mounted. Attempting to remount..." "$LOG_FILE"
   "${PLEX_CODE_DIR}/scripts/cycle-mount.sh"
-  "$SLEEP_BIN" 5
+  "$SLEEP_BIN" 2
 
-  RECOUNT=$("$FIND_BIN" "${DEC_DIR_LOCAL}" -maxdepth 1 -type d | wc -l)
+  RECOUNT=$("$FIND_BIN" "$DEC_DIR_LOCAL" -maxdepth 1 -type d | wc -l)
 
   if [ "$RECOUNT" -eq "1" ]; then
     log_fatal "Drives could not be remounted. Check the logs!" "$LOG_FILE"
   else
-    log_info "Drives mounted successfully. Everything is back to normal." " $LOG_FILE"
+    log_info "Drives mounted successfully. Everything is back to normal." "$LOG_FILE"
   fi
 else
   log_info "Drives are mounted. Everything is OK." "$LOG_FILE"
