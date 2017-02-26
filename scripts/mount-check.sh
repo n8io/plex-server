4ad7a6a82a07d799f23cc73d2c077fd303295909
Nate Clark <n8@n8io.com>
2017-02-26 02:34:29 -0500

    cycle back in the mix

--- scripts/mount-check.sh
+++ scripts/mount-check.sh
@@ -10,6 +10,14 @@ COUNT=$(find "${DEC_DIR_LOCAL}" -maxdepth 1 -type d | wc -l)
 if [ "$COUNT" -eq "1" ]; then
   echo "$(date) CRITICAL: Drives are not mounted. Need to remount!" | "$TEE_BIN" -a "$LOG_FILE"
   "${PLEX_CODE_DIR}/scripts/cycle-mount.sh"
+
+  RECOUNT=$(find "${DEC_DIR_LOCAL}" -maxdepth 1 -type d | wc -l)
+
+  if [ "$RECOUNT" -eq "1" ]; then
+    echo "$(date) FATAL: Drives could not be remounted. Check the logs!" | "$TEE_BIN" -a "$LOG_FILE"
+  else
+    echo "$(date)     INFO: Drives mounted successfully. Everything is back to normal." | "$TEE_BIN" -a "$LOG_FILE"
+  fi
 else
   echo "$(date)     INFO: Drives are mounted. Everything is OK." | "$TEE_BIN" -a "$LOG_FILE"
 fi
