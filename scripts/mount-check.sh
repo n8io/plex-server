6b8a7f09647ee0e6bdfeebe24beecc0af7385738
Nate Clark <n8@n8io.com>
2017-02-26 02:28:22 -0500

    cycle back in the mix

--- scripts/mount-check.sh
+++ scripts/mount-check.sh
@@ -6,9 +6,8 @@ PLEX_CODE_DIR=$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")
 TEE_BIN="/usr/bin/tee"
 LOG_FILE="${PLEX_CODE_DIR}/logs/mount-check.log"
 COUNT=$(find "${DEC_DIR_LOCAL}" -maxdepth 1 -type d | wc -l)
-COUNT=$((COUNT+1-1))
 
-if [ $COUNT -eq 0 ]; then
+if [ "$COUNT" -eq "0" ]; then
   echo "$(date) CRITICAL: Drives are not mounted. Need to remount!" | "$TEE_BIN" -a "$LOG_FILE"
   "${PLEX_CODE_DIR}/scripts/cycle-mount.sh"
 else
