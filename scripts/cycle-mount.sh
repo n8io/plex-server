c9e1e94c77ececc50b0ac94af94d3ce161cca47e
Nate Clark <n8@n8io.com>
2017-02-25 23:39:12 -0500

    Fix bug in mount

--- scripts/cycle-mount.sh
+++ scripts/cycle-mount.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SCREEN_BIN="/usr/bin/screen"
-DEF_PLEX_CODE_DIR="$([ ! -z "$PLEX_CODE_DIR" ] && echo "$PLEX_CODE_DIR" || echo "/plex-server/scripts")"
-DEF_LOGS_DIR="$([ ! -z "$PLEX_CODE_DIR" ] && echo "${PLEX_CODE_DIR}/logs" || echo "/plex-server/logs")"
+DEF_PLEX_CODE_DIR="$([ ! -z "$PLEX_CODE_DIR" ] && echo "$PLEX_CODE_DIR" || echo "/plex-server")"
+DEF_LOGS_DIR="$([ ! -z "$PLEX_CODE_DIR" ] && echo "${PLEX_CODE_DIR}/logs" || echo "${DEF_PLEX_CODE_DIR}/logs")"
 DEF_LOG_FILE="rclone-mount.log"
 
 PLEX_CODE_DIR="${1:-$DEF_PLEX_CODE_DIR}"
@@ -10,11 +10,7 @@ LOG_FILE="${LOGS_DIR}/${3:-$DEF_LOG_FILE}"
 
 mkdir -p "$LOGS_DIR"
 
-echo "
-SCREEN_BIN=$SCREEN_BIN
-PLEX_CODE_DIR=$PLEX_CODE_DIR
-LOG_FILE=$LOG_FILE
-" > "$LOG_FILE"
+echo "$(date)" > "$LOG_FILE"
 
 echo "Unmounting..." >> "$LOG_FILE"
 "$SCREEN_BIN" -ls | grep ".rclone-mount" | cut -d. -f1 | awk '{print $1}' | xargs kill 2>/dev/null || true
