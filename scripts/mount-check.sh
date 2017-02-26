907be2d44ee31e6f32fc4e2c5d5b88bdc2e2b7d7
Nate Clark <n8@n8io.com>
2017-02-26 03:11:42 -0500

    cycle back in the mix

--- scripts/mount-check.sh
+++ scripts/mount-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DIR="$(dirname $0)"
 PLEX_CODE_DIR=$(dirname "$DIR")
 . "${PLEX_CODE_DIR}/.env"
 
--- scripts/mount.sh
+++ scripts/mount.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DIR="$(dirname $0)"
 PLEX_CODE_DIR=$(dirname "$DIR")
 . "${PLEX_CODE_DIR}/.env"
 
