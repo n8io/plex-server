259cb5a46836673a45ad5da8b0d4fecab879f5d3
Nate Clark <n8@n8io.com>
2017-03-28 13:33:15 -0400

    🐛Fix bad path

--- addons/extended-personal-media-scanner.sh
+++ addons/extended-personal-media-scanner.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-/cd /tmp
+SCANNERS_DIR="/home/plexmediaserver/Library/Application Support/Plex Media Server/Scanners"
+cd /tmp
 wget https://bitbucket.org/mjarends/plex-scanners/get/master.zip
 unzip master.zip -d /tmp
-rm master.zip
 mv mjarends-plex-scanners-*/ /tmp/plex-scanners
-cp -R /tmp/plex-scanners/* /home/plexmediaserver/Library/Application Support/Plex Media Server/Scanners/
+mkdir -p "$SCANNERS_DIR"
+cp -R /tmp/plex-scanners/* "$SCANNERS_DIR"
 rm -rf /tmp/master.zip /tmp/plex-scanners
-service plexmediaserver restart
 set +e
